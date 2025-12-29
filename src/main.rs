6f49881b16312350aefbfa2d497c20228b84c2fe
aemonge <andres@aemonge.com>
2025-12-29 14:36:26 +0100

    feat: Including the char position of the needle

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ enum MyErrors {
     MissingArgsError,
 }
 
+fn print_found(line: &str, line_no: usize, char_no: usize, file: &str, query: &str) {
+    let colored_line = line.replace(query, &query.bright_yellow().to_string());
+    println!(
+        "{}:{}:{} {}",
+        file.bold(),
+        (line_no + 1).to_string().bold(),
+        (char_no).to_string().bold(),
+        colored_line
+    );
+}
+
 // impl From<std::io::Error> for MyErrors {
 //     fn from(e: std::io::Error) -> Self {
 //         dbg!(&e);
@@ -15,8 +26,6 @@ enum MyErrors {
 // }
 
 fn single_search(query: &String, file: &String) -> Result<(), MyErrors> {
-    println!("Searching for /{}/ @ {}", query, file);
-
     let lines = fs::read_to_string(file).map_err(|_| MyErrors::FileReadError(file.clone()))?;
     for (line_no, line) in lines.lines().enumerate() {
         // match line.find(query) {
@@ -28,14 +37,11 @@ fn single_search(query: &String, file: &String) -> Result<(), MyErrors> {
         // if let Some(_) = line.find(query) {
         //     println!("found it at {}", line);
         // }
-        if line.contains(query) {
-            let colored_line = line.replace(query, &query.bright_yellow().to_string());
-            println!(
-                "{}:{} {}",
-                file.bold(),
-                (line_no + 1).to_string().bold(),
-                colored_line
-            );
+        // if line.contains(query) {
+        //     print_found(line, line_no, 0, file, query);
+        // }
+        if let Some(char_no) = line.find(query) {
+            print_found(line, line_no, char_no, file, query);
         }
     }
     Ok(())
