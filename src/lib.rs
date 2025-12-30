0bd1f647d64eede545a3182fce6615467f063e8b
aemonge <andres@aemonge.com>
2025-12-30 12:11:12 +0100

    feat: Including unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,23 @@ pub mod types;
 use crate::types::MyErrors;
 use std::fs;
 
-fn single_search(query: &String, file: &String, quiet: bool) -> Result<usize, MyErrors> {
-    let mut matchs = 0;
-    let lines = fs::read_to_string(file).map_err(|_| MyErrors::FileReadError(file.clone()))?;
+fn search_on_lines(query: &str, file_name: &str, lines: &str) -> Vec<String> {
+    let mut found: Vec<String> = vec![];
     for (line_no, line) in lines.lines().enumerate() {
-        matchs += 1;
-        if !quiet && let Some(char_no) = line.find(query) {
-            pretty::print_found(line, line_no, char_no, file, query);
+        if let Some(char_no) = line.find(query) {
+            found.push(pretty::print_found(
+                line, line_no, char_no, file_name, query,
+            ));
         }
     }
+    found
+}
 
-    Ok(matchs)
+fn single_search(query: &str, file_name: &String) -> Result<Vec<String>, MyErrors> {
+    let lines =
+        fs::read_to_string(file_name).map_err(|_| MyErrors::FileReadError(file_name.clone()))?;
+
+    Ok(search_on_lines(query, file_name, &lines))
 }
 
 pub fn search(query: String, files: Vec<String>, quiet: bool) -> Result<usize, MyErrors> {
@@ -22,11 +28,61 @@ pub fn search(query: String, files: Vec<String>, quiet: bool) -> Result<usize, M
         return Err(MyErrors::MissingArgsError);
     }
     let mut files = files.clone();
-    let mut matchs = 0;
+    let mut matchs: Vec<String> = vec![];
 
     while let Some(file) = files.pop() {
-        matchs += single_search(&query, &file, quiet)?;
+        let e = single_search(&query, &file)?;
+        matchs = [matchs, e].concat();
     }
 
-    Ok(matchs)
+    let found_n = matchs.len();
+
+    if !quiet {
+        for found in matchs {
+            println!("{}", found);
+        }
+    }
+
+    Ok(found_n)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result_on_search_on_lines() {
+        let query = "safe";
+        let file_name = "test.tmp";
+        let contents = "\
+Rust
+safe, fast, productive.
+Pick three.";
+        let result = search_on_lines(query, file_name, contents);
+        assert_eq!(result.len(), 1);
+    }
+
+    #[test]
+    fn no_result_on_search_on_lines() {
+        let query = "duck";
+        let file_name = "test.tmp";
+        let contents = "\
+Rust
+safe, fast, productive.
+Pick three.";
+        let result = search_on_lines(query, file_name, contents);
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn three_result_on_search_on_lines() {
+        let query = "crab";
+        let file_name = "test.tmp";
+        let contents = "\
+Rust, the crab
+safe, fast, productive as a crab.
+Pick three from crab.";
+        let result = search_on_lines(query, file_name, contents);
+        assert_eq!(result.len(), 3);
+    }
 }
--- src/pretty.rs
+++ src/pretty.rs
@@ -1,12 +1,12 @@
 use owo_colors::OwoColorize; // Powers Up Strings with colors
 
-pub fn print_found(line: &str, line_no: usize, char_no: usize, file: &str, query: &str) {
+pub fn print_found(line: &str, line_no: usize, char_no: usize, file: &str, query: &str) -> String {
     let colored_line = line.replace(query, &query.bright_yellow().to_string());
-    println!(
+    format!(
         "{}:{}:{} {}",
         file.bold(),
         (line_no + 1).to_string().bold(),
         (char_no).to_string().bold(),
         colored_line
-    );
+    )
 }
